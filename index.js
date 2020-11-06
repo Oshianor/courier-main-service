e835048995de3a49b2c61cf2059bf70da3548524
oshianor <abundanceoshianor@gmail.com>
2020-11-06 18:16:01 +0100

    dev backup

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const config = require("config");
-const port = process.env.PORT || config.get("application.port");
 require("winston-mongodb");
 require("./startup/aws");
 require("./startup/logger");
 require("./startup/db");
 require("./startup/routes");
-
-// const { http } = require("./startup/socket");
-
-// http.listen(port, () => console.error(`listening on http://localhost:${port}`));

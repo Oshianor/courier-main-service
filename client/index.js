9a780f00c67e1bba7c2b657bb7a68c60b2818a20
Ndifreke Friday <ndiecodes@gmail.com>
2020-11-11 18:05:20 +0100

    return newRider

--- client/index.js
+++ client/index.js
@@ -1,12 +1,10 @@
 const io = require("socket.io-client");
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
 
-// const ioClient = io.connect("https://dev.api.logistics.churchesapp.com");
+const ioClient = io.connect("https://dev.api.logistics.churchesapp.com");
 
-const ioClient = io.connect(`http://localhost:${port}`);
+// const ioClient = io.connect(`http://localhost:${port}`);
 
 ioClient.on("connect", () => console.info("connected"));
 
-ioClient.on("newEntry", (msg) => console.info(msg));
-
-ioClient.on("newOrder", (msg) => console.info(msg));
+ioClient.on("NEW_ENTRY", (entry) => console.info(entry));

2c7003e336c111baf735a0d245325cdf1a3d9fda
yemipedro07 <yemipedro07@gmail.com>
2021-01-12 09:03:26 +0100

    update permission on update

--- services/auth.js
+++ services/auth.js
@@ -285,7 +285,7 @@ class AuthSerivice {
         if (!account || account.role === "owner") {
           return reject({ code: 400, msg: "You do not have permission to disable this account" })
         }
-        if (account.role === "branch" && enterprise.type !== "HQ") {
+        if (account.role === "branch" && enterprise.type !== "owner") {
           return reject({ code: 400, msg: "You do not have permission to disable this account" })
         }
         const response = await axios.patch(

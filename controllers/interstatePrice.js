29c2940b7249f828deb42398990a806c1ce1a627
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-04-01 21:22:40 +0100

    cleaned code base

--- controllers/interstatePrice.js
+++ controllers/interstatePrice.js
@@ -6,82 +6,77 @@ const interstatePriceService = require("../services/interstatePriceService")
 
 
 exports.create = async (req, res, next) => {
-    try {
-        // validate request
-        const { error } = validateInterstatePrice(req.body);
-        if (error)
-            return JsonResponse(res, 400, error.details[0].message);
+  try {
+    // validate request
+    const { error } = validateInterstatePrice(req.body);
+    if (error)
+      return JsonResponse(res, 400, error.details[0].message);
 
-        const iTSP = new interstatePriceService();
-        await iTSP.create(req.body)
-        JsonResponse(res, 200, MSG_TYPES.CREATED);
-        return;
-    } catch (error) {
-        next(error);
+    const interStateInstance = new interstatePriceService();
+    await interStateInstance.create(req.body)
+    return JsonResponse(res, 200, MSG_TYPES.CREATED);
+  } catch (error) {
+    next(error);
 
-    }
+  }
 };
 
 
 exports.getAll = async (req, res, next) => {
-    try {
-        const iTSP = await model.find({})
+  try {
+    const interStateInstance = await model.find({})
 
-        JsonResponse(res, 200, MSG_TYPES.FETCHED, iTSP);
-        return;
-    } catch (error) {
-        next(error);
+    return JsonResponse(res, 200, MSG_TYPES.FETCHED, interStateInstance);
+  } catch (error) {
+    next(error);
 
-    }
+  }
 };
 
 
 exports.getById = async (req, res, next) => {
-    try {
-        const iTSP = new interstatePriceService()
-        let data = await iTSP.getById(req.query.id)
-        JsonResponse(res, 200, MSG_TYPES.FETCHED, data);
-        return;
-    } catch (error) {
-        next(error);
+  try {
+    const interStateInstance = new interstatePriceService()
+    let data = await interStateInstance.getById(req.query.id)
+    return JsonResponse(res, 200, MSG_TYPES.FETCHED, data);
+  } catch (error) {
+    next(error);
 
-    }
+  }
 };
 
 exports.delete = async (req, res, next) => {
-    try {
-        const iTSP = new interstatePriceService()
-        let data = await iTSP.delete(req.query.id)
-        JsonResponse(res, 200, MSG_TYPES.DELETED, data);
-        return;
-    } catch (error) {
-        next(error);
+  try {
+    const interStateInstance = new interstatePriceService()
+    let data = await interStateInstance.delete(req.query.id)
+    return JsonResponse(res, 200, MSG_TYPES.DELETED, data);
+  } catch (error) {
+    next(error);
 
-    }
+  }
 };
 
 exports.update = async (req, res, next) => {
-    try {
-        // validate request
-        const { error } = validateUpdateInterstatePrice(req.body);
-        if (error)
-            return JsonResponse(res, 400, error.details[0].message, null, null);
+  try {
+    // validate request
+    const { error } = validateUpdateInterstatePrice(req.body);
+    if (error)
+      return JsonResponse(res, 400, error.details[0].message, null, null);
 
-        // check if account exist
-        // const admin = await Admin.findOne({ _id: req.user.id, status: "active" });
-        // if (!admin)
-        //     return JsonResponse(res, 400, MSG_TYPES.ACCESS_DENIED, null, null);
+    // check if account exist
+    // const admin = await Admin.findOne({ _id: req.user.id, status: "active" });
+    // if (!admin)
+    //     return JsonResponse(res, 400, MSG_TYPES.ACCESS_DENIED, null, null);
 
-        // create new account record
-        const detail = await model.findById(req.query.id);
-        if (!detail)
-            return JsonResponse(res, 400, MSG_TYPES.NOT_FOUND, null, null);
+    // create new account record
+    const detail = await model.findById(req.query.id);
+    if (!detail)
+      return JsonResponse(res, 400, MSG_TYPES.NOT_FOUND, null, null);
 
-        await model.updateOne(req.body);
+    await model.updateOne(req.body);
 
-        JsonResponse(res, 200, MSG_TYPES.UPDATED, null, null);
-        return;
-    } catch (error) {
-        next(error);
-    }
+    return JsonResponse(res, 200, MSG_TYPES.UPDATED, null, null);
+  } catch (error) {
+    next(error);
+  }
 };
--- services/interstatePriceService.js
+++ services/interstatePriceService.js
@@ -1,45 +1,55 @@
-const interstateModel = require("../models/interstatePrice");
+const InterstatePrice = require("../models/interstatePrice");
 
 class interstatePriceService {
-    create = (options) => {
-        return new Promise(async (resolve, reject) => {
-            const checkExist = await interstateModel.findOne({
-                $and: [
-                    { originCountry: options.originCountry },
-                    { originState: options.originState },
-                    { destinationState: options.destinationState },
-                    { destinationCountry: options.destinationCountry },
-                ],
-            });
-            if (!checkExist) {
-                let createData = await interstateModel.create(options);
-                resolve(createData);
-            }
-            reject({ code: 400, msg: "Inputs already exists" });
+  create = (options) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const checkExist = await InterstatePrice.findOne({
+          $and: [
+            { originCountry: options.originCountry },
+            { originState: options.originState },
+            { destinationState: options.destinationState },
+            { destinationCountry: options.destinationCountry },
+          ],
         });
-    };
+        if (!checkExist) {
+          let createData = await InterstatePrice.create(options);
+          resolve(createData);
+        }
+        reject({ code: 400, msg: "Inputs already exists" });
+      } catch (error) {
+        reject({ code: 500, msg: "something went wrong" });
+      }
+    });
+  };
 
-    getById = (id) => {
-        return new Promise(async (resolve, reject) => {
-            const findData = await interstateModel.findById({ _id: id });
-            if (!findData) {
-                reject({ code: 404, msg: "Data not found" });
-            }
-            resolve(findData);
-        });
-    };
+  getById = (id) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const findData = await InterstatePrice.findById({ _id: id });
+        if (!findData) {
+          return reject({ code: 404, msg: "Data not found" });
+        }
+        resolve(findData);
+      } catch (error) {
+        reject({ code: 500, msg: "something went wrong" });
+      }
+    });
+  };
 
-    delete = (id) => {
-        return new Promise(async (resolve, reject) => {
-            const deleteData = await interstateModel.findByIdAndDelete({ _id: id });
-            if (deleteData) {
-                resolve(deleteData);
-            }
-            reject({ code: 404, msg: "Error deleting data" });
-
-
-        });
-    }
+  delete = (id) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const deleteData = await InterstatePrice.findByIdAndDelete({ _id: id });
+        if (deleteData) {
+          resolve(deleteData);
+        }
+        reject({ code: 404, msg: "Error deleting data" });
+      } catch (error) {
+        reject({ code: 500, msg: "something went wrong" });
+      }
+    });
+  };
 }
 
 module.exports = interstatePriceService;

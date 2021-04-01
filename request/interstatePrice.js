b6a2bc51c5ea16d5a049cc6dbe717a91926d3c95
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-04-01 16:42:16 +0100

    created interstate price validator

---
+++ request/interstatePrice.js
@@ -0,0 +1,38 @@
+const Joi = require("joi");
+
+function validateInterstatePrice(data) {
+    const Schema = Joi.object().keys({
+        originCountry: Joi.string().label("Origin country").required(),
+        originState: Joi.string().label("Origin State").required(),
+        destinationCountry: Joi.string().label("destination country").required(),
+        destinationState: Joi.string().label("destination State").required(),
+        company: Joi.string().regex(/^[0-9a-fA-F]{24}$/).label("Company").required(),
+        organization: Joi.string().regex(/^[0-9a-fA-F]{24}$/).label("Organization").required(),
+        price: Joi.string().label("Price").required(),
+        currency: Joi.string().label("Currency").required(),
+        source: Joi.string().label("Source").required()
+    });
+
+    return Schema.validate(data);
+}
+
+function validateUpdateInterstatePrice(data) {
+    const Schema = Joi.object().keys({
+        originCountry: Joi.string().label("Origin country").required(),
+        originState: Joi.string().label("Origin State").required(),
+        destinationCountry: Joi.string().label("destination country").required(),
+        destinationState: Joi.string().label("destination State").required(),
+        company: Joi.string().regex(/^[0-9a-fA-F]{24}$/).label("Company").required(),
+        organization: Joi.string().regex(/^[0-9a-fA-F]{24}$/).label("Organization").required(),
+        price: Joi.string().label("Price").required(),
+        currency: Joi.string().label("Currency").required(),
+        source: Joi.string().label("Source").required()
+    });
+
+    return Schema.validate(data);
+}
+
+module.exports = {
+    validateInterstatePrice,
+    validateUpdateInterstatePrice
+};

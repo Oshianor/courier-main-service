d12836ab024752fd228d482572cec8e209fcf04e
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-04-02 13:05:55 +0100

    created a crude company interstate price

--- services/interstatePriceService.js
+++ services/interstatePriceService.js
@@ -1,5 +1,5 @@
 const InterstatePrice = require("../models/interstatePrice");
-
+const Company = require('../models/company');
 class interstatePriceService {
   create = (options) => {
     return new Promise(async (resolve, reject) => {
@@ -23,6 +23,38 @@ class interstatePriceService {
     });
   };
 
+  createCompanyInterstatePrice = (id, options) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const getCompanyDetails = await Company.findById({ _id: id });
+        const checkExist = await InterstatePrice.findOne({
+          $and: [
+            { originCountry: getCompanyDetails.country },
+            { originState: getCompanyDetails.state },
+            { destinationState: options.destinationState },
+            { destinationCountry: options.destinationCountry },
+            { company: getCompanyDetails._id },
+            { organization: getCompanyDetails.organization }
+          ],
+        });
+        if (!checkExist) {
+          let data = {
+            originCountry: getCompanyDetails.country,
+            originState: getCompanyDetails.state,
+            company: getCompanyDetails._id,
+            organization: getCompanyDetails.organization
+          }
+          let savedData = Object.assign(options, data)
+          let createData = await InterstatePrice.create(savedData);
+          resolve(createData);
+        }
+        reject({ code: 400, msg: "Inputs already exists" });
+      } catch (error) {
+        reject({ code: 500, msg: "something went wrong" })
+      }
+    })
+  }
+
   getById = (id) => {
     return new Promise(async (resolve, reject) => {
       try {
@@ -32,6 +64,21 @@ class interstatePriceService {
         }
         resolve(findData);
       } catch (error) {
+        reject({ code: 500, msg: "something went wrong" });
+      }
+    });
+  };
+
+  getCompanyInterStatePriceById = (companyId, option) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const findData = await InterstatePrice.findOne({ $and: [{ company: companyId }, { _id: option.id }] });
+        if (!findData) {
+          return reject({ code: 404, msg: "Data not found" });
+        }
+        resolve(findData);
+      } catch (error) {
+        console.log(error)
         reject({ code: 500, msg: "something went wrong" });
       }
     });
@@ -50,6 +97,22 @@ class interstatePriceService {
       }
     });
   };
+
+  deleteCompanyInterstatePrice = (companyId, option) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const deleteData = await InterstatePrice.findOneAndDelete({ $and: [{ company: companyId }, { _id: option.id }] });
+        if (deleteData) {
+          resolve(deleteData);
+        }
+        reject({ code: 404, msg: "Error deleting data" });
+      } catch (error) {
+        reject({ code: 500, msg: "something went wrong" });
+      }
+    });
+  };
+
+
 }
 
 module.exports = interstatePriceService;

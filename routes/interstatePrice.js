63e0a8635ed142307ee559e9da2474c0d2a3c151
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-04-02 13:05:47 +0100

    created a crude  routes for company interstate price

--- routes/interstatePrice.js
+++ routes/interstatePrice.js
@@ -4,9 +4,14 @@ const controller = require("../controllers");
 const { hasRole, ROLES, Auth } = require("../middlewares/auth");
 
 router.post("/create", Auth, hasRole([ROLES.SUPER_ADMIN, ROLES.ADMIN]), controller.interstatePrice.create);
+router.post("/company/create", Auth, controller.interstatePrice.createCompanyInterstatePrice);
 router.get("/all", Auth, hasRole([ROLES.SUPER_ADMIN, ROLES.ADMIN]), controller.interstatePrice.getAll);
+router.get("/company/all", Auth, controller.interstatePrice.getAllCompanyInterstatePrice);
 router.get("/single", Auth, hasRole([ROLES.SUPER_ADMIN, ROLES.ADMIN]), controller.interstatePrice.getById);
+router.get("/company/single", Auth, controller.interstatePrice.getCompanyInterStatePriceById);
 router.delete("/delete", Auth, hasRole([ROLES.SUPER_ADMIN, ROLES.ADMIN]), controller.interstatePrice.delete);
+router.delete("/company/delete", Auth, controller.interstatePrice.deleteCompanyInterstatePrice);
 router.put("/edit", Auth, hasRole([ROLES.SUPER_ADMIN, ROLES.ADMIN]), controller.interstatePrice.update);
+router.put("/company/edit", Auth, controller.interstatePrice.updateCompanyInterstatePrice);
 
 module.exports = router;

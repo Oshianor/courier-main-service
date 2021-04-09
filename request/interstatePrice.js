d8f16a490d6ef5587b573f5026d689714d1d6cbc
ochoko chukwuebuka peter <buka4chocksy@gmail.com>
2021-04-09 03:35:58 +0100

    modified company interstate price validator and admin interstate validator

--- request/interstatePrice.js
+++ request/interstatePrice.js
@@ -32,7 +32,21 @@ function validateUpdateInterstatePrice(data) {
     originState: Joi.string().label("Origin State").required(),
     destinationCountry: Joi.string().label("destination country").required(),
     destinationState: Joi.string().label("destination State").required(),
-    price: Joi.string().label("Price").required(),
+    price: Joi.number().label("Price").required(),
+    location: Joi.array()
+      .items({
+        address: Joi.string().label("Address").required(),
+        name: Joi.string().label("Name").required(),
+        email: Joi.string().email().max(50).label("Email").optional(),
+        phoneNumber: Joi.string()
+          .regex(/^[1-9][0-9]{9}$/)
+          .required()
+          .messages({
+            "string.pattern.base": `Phone Number can't not have a leading zero (0)`,
+          }),
+        countryCode: Joi.string().max(5).required(),
+      })
+      .required(),
   });
 
   return Schema.validate(data);
@@ -42,7 +56,21 @@ function validateCompanyInterstatePrice(data) {
   const Schema = Joi.object().keys({
     destinationCountry: Joi.string().label("destination country").required(),
     destinationState: Joi.string().label("destination State").required(),
-    price: Joi.string().label("Price").required(),
+    price: Joi.number().label("Price").required(),
+    location: Joi.array()
+      .items({
+        address: Joi.string().label("Address").required(),
+        name: Joi.string().label("Name").required(),
+        email: Joi.string().email().max(50).label("Email").optional(),
+        phoneNumber: Joi.string()
+          .regex(/^[1-9][0-9]{9}$/)
+          .required()
+          .messages({
+            "string.pattern.base": `Phone Number can't not have a leading zero (0)`,
+          }),
+        countryCode: Joi.string().max(5).required(),
+      })
+      .required(),
   });
 
   return Schema.validate(data);
@@ -52,12 +80,27 @@ function validateUpdateCompanyInterstatePrice(data) {
   const Schema = Joi.object().keys({
     destinationCountry: Joi.string().label("destination country").required(),
     destinationState: Joi.string().label("destination State").required(),
-    price: Joi.string().label("Price").required(),
+    price: Joi.number().label("Price").required(),
     id: Joi.string()
       .regex(/^[0-9a-fA-F]{24}$/)
       .label("id")
       .required(),
-  });
+    location: Joi.array()
+      .items({
+        address: Joi.string().label("Address").required(),
+        name: Joi.string().label("Name").required(),
+        email: Joi.string().email().max(50).label("Email").optional(),
+        phoneNumber: Joi.string()
+          .regex(/^[1-9][0-9]{9}$/)
+          .required()
+          .messages({
+            "string.pattern.base": `Phone Number can't not have a leading zero (0)`,
+          }),
+        countryCode: Joi.string().max(5).required(),
+      })
+      .required(),
+  })
+
 
   return Schema.validate(data);
 }
